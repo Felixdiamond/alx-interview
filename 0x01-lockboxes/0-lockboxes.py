ed86c32e92f5e5d5e2cd5b3296f4a9a1f2baa401
Felixdiamond <diamondfelix006@gmail.com>
2023-07-04 15:29:47 -0700

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (List[List[int]]): A list of lists, where each inner list represents a box and contains the keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    keys = [0]
+    opened = [False] * len(boxes)
+    opened[0] = True
+    while keys:
+        current_key = keys.pop(0)
+        for key in boxes[current_key]:
+            if key < len(boxes) and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+    return all(opened)
+
